37b6ae8bd39cfd189b3eb10f52b12208a4122b94
Ygor Geurts <Ygor.Geurts@rtl.nl>
2020-10-22 23:50:47 +0200

    day6 part2

--- day6/Program.fs
+++ day6/Program.fs
@@ -1,4 +1,5 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
 type Coordinate = int * int
 
@@ -9,7 +10,7 @@ type Instruction =
     | Off of Grid
     | Toggle of Grid
 
-type Lights = Map<Coordinate, bool>
+type Lights<'a> = Map<Coordinate, 'a>
 
 let input = File.ReadAllLines(@"input.txt")
 
@@ -27,33 +28,50 @@ let parseLine (line: string) =
 
 let instructions = input |> Seq.map parseLine
 
-let lights =
+let lights<'a> initial =
     Seq.allPairs [ 0 .. 999 ] [ 0 .. 999 ]
-    |> Seq.fold (fun (lights': Lights) coordinate -> lights'.Add(coordinate, false)) Map.empty<Coordinate, bool>
+    |> Seq.fold (fun (lights': Lights<'a>) coordinate -> lights'.Add(coordinate, initial)) Map.empty<Coordinate, 'a>
 
-let switchLights (lights: Lights) grid fn =
+let changeLights (lights: Lights<'a>) grid changeFn =
     let (a, b), (c, d) = grid
     [ a .. c ]
-    |> Seq.fold (fun (lights': Lights) x ->
-        [ b .. d ] |> Seq.fold (fun lights'' y -> lights''.Add((x, y), fn lights''.[(x, y)])) lights') lights
+    |> Seq.fold (fun (lights': Lights<'a>) x ->
+        [ b .. d ] |> Seq.fold (fun lights'' y ->
+            lights''.Add((x, y), changeFn lights''.[(x, y)])) lights') lights
 
 let applyInstruction lights instruction =
     match instruction with
-    | On grid -> switchLights lights grid (fun _ -> true)
-    | Off grid -> switchLights lights grid (fun _ -> false)
-    | Toggle grid -> switchLights lights grid (fun state -> not state)
+    | On grid -> changeLights lights grid (fun _ -> true)
+    | Off grid -> changeLights lights grid (fun _ -> false)
+    | Toggle grid -> changeLights lights grid (fun state -> not state)
 
-let setup lights instructions =
-    instructions |> Seq.fold applyInstruction lights
+let applyBrightnessInstruction lights instruction =
+    match instruction with
+    | On grid -> changeLights lights grid (fun state -> state + 1)
+    | Off grid -> changeLights lights grid (fun state -> Math.Max(0, (state - 1)))
+    | Toggle grid -> changeLights lights grid (fun state -> state + 2)
 
-let numLightsLit lights =
+let setup lights instructions instructionFn =
+    instructions |> Seq.fold instructionFn lights
+
+let totalLightsLit lights =
     lights
     |> Map.filter (fun _ state -> state)
     |> Map.count
 
+let totalBrightness lights =
+    lights
+    |> Map.toList
+    |> Seq.sumBy (fun (_, brightness) -> brightness)
+    
 [<EntryPoint>]
 let main argv =
-    setup lights instructions
-    |> numLightsLit
+    setup (lights false) instructions applyInstruction
+    |> totalLightsLit
     |> printfn "Part 1. Number of lights lit: %i"
+
+    setup (lights 0) instructions applyBrightnessInstruction
+    |> totalBrightness
+    |> printfn "Part 2. Number of lights lit: %i"
+
     0
